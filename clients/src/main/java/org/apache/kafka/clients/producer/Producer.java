60a0c53ff74f83307f6c4fe5eba81b310fafbbc3
fpj <fpj@apache.org>
2016-06-13 18:26:14 +0100

    Initial set of API changes for idempotent sends with guarantees across producer instances.

--- clients/src/main/java/org/apache/kafka/clients/producer/Producer.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/Producer.java
@@ -35,6 +35,19 @@ import org.apache.kafka.common.MetricName;
 public interface Producer<K, V> extends Closeable {
 
     /**
+     * Initializes a session for this producer instance so that
+     * internal retries are idempotent. The application is supposed
+     * to persist the session identifier it returns so that it can
+     * be used to restore the session for recovery.
+     *
+     * If the session is being initialized for the first time, then
+     * we pass null as the id parameter.
+     * @param id
+     * @return A session id so that the application can ruse upon recovery.
+     */
+    public SessionId initializeSession(SessionId id);
+
+    /**
      * Send the given record asynchronously and return a future which will eventually contain the response information.
      * 
      * @param record The record to send
