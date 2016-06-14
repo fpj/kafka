bb8cee7b8ac7b79fa1aae577d6c625b035541fda
fpj <fpj@apache.org>
2016-06-14 09:45:56 +0100

    Updates to session handle.

--- clients/src/main/java/org/apache/kafka/clients/producer/SessionHandle.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/SessionHandle.java
@@ -27,7 +27,7 @@ public class SessionHandle {
      * @param pid
      */
     SessionHandle(long pid) {
-        this(appState, null);
+        this(pid, null);
     }
 
     SessionHandle(long pid, byte[] appState) {
@@ -68,6 +68,6 @@ public class SessionHandle {
     }
 
     private void restore(byte[] idBytes) {
-        // extract pid
+        // extract pid and appstate
     }
 }
