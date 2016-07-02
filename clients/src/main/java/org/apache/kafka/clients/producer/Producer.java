ac0e7ec2eaf276db1f40185ab33b10759a151410
fpj <fpj@apache.org>
2016-07-02 14:40:46 +0100

    Made pid a string.

--- clients/src/main/java/org/apache/kafka/clients/producer/Producer.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/Producer.java
@@ -56,7 +56,7 @@ public interface Producer<K, V> extends Closeable {
      * @return A future that eventually returns and updated
      *         ProducerIdentifier instance.
      */
-    public Future<ProducerIdentifier> initPid(ProducerIdentifier pid, CommitCallback cb);
+    public Future<String> initPid(String pid, CommitCallback cb);
 
     /**
      * Send the given record asynchronously and return a future which will eventually contain the response information.
